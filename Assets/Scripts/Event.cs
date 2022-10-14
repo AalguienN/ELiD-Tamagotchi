f5ca194f823e4fe10ca5f6d7e844fa6b0aed37a8
ByteDevEs <ByteDevEs@noreply.github.com>
2022-10-14 16:50:58 +0200

    Events

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,6 @@
+public class CalendarEvent {
+    public static string id;
+    public string eventName;
+    public int month;
+    public int day;
+}
---
+++ Assets/Scripts/MonthlyCalendar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonthlyCalendar
+{
+    public List<CalendarEvent> events;
+
+    public CalendarEvent string
+}
