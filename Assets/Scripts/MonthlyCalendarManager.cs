cd1f1b75e65ccf71f2eb4d9a5e5337692c140523
Alejandro Navarro García <ANAVGAR@upv.edu.es>
2022-10-15 13:32:20 +0200

    Monthly Calendar Removed

--- Assets/Scripts/MonthlyCalendarManager.cs
+++ Assets/Scripts/MonthlyCalendarManager.cs
@@ -5,24 +5,58 @@ using UnityEngine;
 
 public class MonthlyCalendarManager : MonoBehaviour
 {
-    public int day;
-    public int month;
+    int savedDay;
+    int savedMonth;
+    int currentDay;
+    int currentMonth;
 
-    MonthlyCalendar calendarController;
+    ArrayList events;
+
+    public GameObject[] daysInCalendarDisplay = new GameObject[35];
 
     // Start is called before the first frame update
     void Start()
     {
-        calendarController = new MonthlyCalendar();
+        events = SaveManager.events;
+        StartupCalendarDisplay();
     }
 
     // Update is called once per frame
     void Update()
     {
         DateTime date = DateTime.Now;
-        day = date.Day;
-        month = date.Month;
+        currentDay = date.Day;
+        currentMonth = date.Month;
+        if(currentDay != savedDay || currentMonth != savedMonth) {
+            savedDay = currentDay;
+            savedMonth = currentMonth;
+        }
+    }
 
-        
+    public CalendarEvent getCalendarEvent(string id) {
+        foreach (CalendarEvent e in events) {
+            if(e.id == id) return e;
+        }
+        return null;
+    }
+
+    // public void addCalendarEvent(CalendarEvent event) {
+    //     SaveManager.addEvent(event);
+    //     UpdateCalendarDisplay();
+    // }
+
+    // public void removeCalendarEvent(CalendarEvent event) {
+    //     SaveManager.removeEvent(event);
+    //     UpdateCalendarDisplay();
+    // }
+
+    public void StartupCalendarDisplay() {
+        foreach(GameObject dayGameObject in daysInCalendarDisplay) {
+            
+        }
+    }
+
+    public void UpdateCalendarDisplay() {
+
     }
 }
