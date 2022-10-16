db701be0dacfff6218ae6970311779423e338aac
david20025010 <93796475+david20025010@users.noreply.github.com>
2022-10-16 19:58:22 +0200

    Update WeeklyCalendar.cs

--- Assets/Scripts/WeeklyCalendar.cs
+++ Assets/Scripts/WeeklyCalendar.cs
@@ -1,4 +1,4 @@
-using System;
+ï»¿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +9,12 @@ using UnityEngine;
 
 Pasan 3 dias, estamos a 19 de octubre
     El dia 16, 17 y 18 se tienen que eliminar del calendario pq como ya han pasado, no nos interesa guardarlos
-    Ademas de eso, hay que generar 3 dias más en el futuro, el dia 24,25 y 26 (creo)
+    Ademas de eso, hay que generar 3 dias mï¿½s en el futuro, el dia 24,25 y 26 (creo)
 
 Luego la opcion extra que dices de machacar los dias futuros es de que haya una probabilidad de x% de que un dia que ya tenga un valor establecido cambie
-    Pero esto ya se hará, es un if con un (random > 0.2) p.ej.
+    Pero esto ya se harï¿½, es un if con un (random > 0.2) p.ej.
 
-La parte de eliminar los dias, lo mejor sería obtener el dia desde el 1 de Enero de 1970 y compararlo por si acaso el jugador juega desde el 25 de diciembre al 4 de enero por ejemplo
+La parte de eliminar los dias, lo mejor serï¿½a obtener el dia desde el 1 de Enero de 1970 y compararlo por si acaso el jugador juega desde el 25 de diciembre al 4 de enero por ejemplo
 */
 
 //Dejo el ejemplo para que no se pierda la idea de lo que hay que hacer >:^)
@@ -25,33 +25,37 @@ public class WeeklyCalendar : MonoBehaviour
     private const float climateChangeProbability = 0.2f; //Porcentaje de probabilidad de que el clima cambie de un dia para otro 
     private float random;
 
-    public void Start() {
-        for(int i = 0; i < 7; i++){
-            week[i].weather = Day.Tiempo.vacio; 
-        }    //Por defecto esta vacio todo.
-    }
-
-    //Funciones para randomizar
-
-    public int getDay() //Obtiene el dia de la semana de Domingo a Lunes de la proxima semana (0-6) 
+    public void Start()
     {
-        return (int)DateTime.Now.DayOfWeek;
+        for (int i = 0; i < 7; i++)
+        {
+            week[i].weather = Day.Tiempo.vacio;//Por defecto estï¿½ vacio todo.
+        }
 
     }
+
+     //Funciones para randomizar
+    public int getDay() //Obtiene el dia de la semana de Domingo a Lunes de la proxima semana (0-6) 
+     {
+        return (int)DateTime.Now.DayOfWeek;
+     }
 
     public float random_climate()
-    {
-        return (float)UnityEngine.Random.Range(0.1f, 1.5f);  //Si es estrictamente mayor al climateChangeProbability se machaca el clima de ese día
-    }
+     {
+        return (float)UnityEngine.Random.Range(0.1f, 1.5f);  //Si es estrictamente mayor al climateChangeProbability se machaca el clima de ese dï¿½a
+     }
 
-
-    //Función para establecer el clima de la semana.
-    public void RandomWeather(Day[] w){
-        for(int day = 0; day < 7; day++){    //Obtienes el día de hoy (0..6) como punto de inicio
-            if(w[day].weather == Day.Tiempo.vacio) {
-                int r = UnityEngine.Random.Range(1,2);
+    //Funcion para establecer el clima
+    public void RandomWeather(Day[] w)
+     {
+         for (int day = 0; day < 7; day++) {
+            if (week[day].weather == Day.Tiempo.vacio)
+            {
+                int r = UnityEngine.Random.Range(1, 2);
                 w[day].weather = (Day.Tiempo)r; //Rellena con sol o lluvia los dias de la semana restantes
             }
-        }
+          }
+     }
     }
-}
+
+
