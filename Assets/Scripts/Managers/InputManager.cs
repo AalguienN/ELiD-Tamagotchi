b214b72827048b07de41a6893699224c86c3cb97
paraxodon <pauperezroy@gmail.com>
2022-10-17 23:58:06 +0200

    Added my own testing scene, some folder organization and started input

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+   public static Slide Slide()
+    {
+        return null; //Aun no he pensado como implementar esto por si alguien lo lee y no le ve sentido
+    }
+}
+
+public class Slide{
+    public Vector2 direction;
+    public float velocity;
+}
