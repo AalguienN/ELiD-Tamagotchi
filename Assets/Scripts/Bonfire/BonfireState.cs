fb1e197886f3ce160736c4962dee927ad6e2d320
Adrián Camacho <adricamachogarcia@gmail.com>
2022-10-19 23:22:24 +0200

    Bonfire upgraded
    Added SaveManager and other improvements

--- Assets/Scripts/Bonfire/BonfireState.cs
+++ Assets/Scripts/Bonfire/BonfireState.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class BonfireState : MonoBehaviour
 {
+    [Header("References")]
+    //For now is empty
+
+    #region Variables
     [Header("TickRate in seconds")]
     public float tickRate = 1f;
     
 
     [Header("Stats")]
     public double hp;
-    public double maxHp = 100;
+    public double maxHp = 1000;
     public states state;
     public globalState weather = globalState.despejado; //Provisional
 
@@ -20,29 +24,40 @@ public class BonfireState : MonoBehaviour
     
     public List<Fuel> fuelList;
 
-    [Header("Testing remove later")]
+    [Header("Testing: remove later \n USAD ESTO COMO BOTONES \n PARA ENCENDER Y APAGAR LA HOGUERA")]
+
     public bool turnOn;
     public bool turnOff;
 
+    #endregion
+
+    public static int seconds;
+
+    #region enums
     //Estado de la hoguera
     public enum states { 
         encendida,
         apagada
     }
-
-    
-
     //Weather, modify in the future to merge with the rest
     public enum globalState { 
         despejado,
         lluvia
     }
-    
+    #endregion
+
+    #region Start_And_Update
     private void Start()
     {
+        turnOn = false; turnOff = false;
+
+        int tiempoFuera = SaveManager.getSecondsSinceLastConexion();
+        
         fuelList = new List<Fuel>();
-        lit();
+        lit(tiempoFuera);
         StartCoroutine(bonfireTick());
+
+        Debug.Log("El juego ha estado cerrado " + Mathf.Abs(tiempoFuera) + " segundos");
     }
     private void Update()
     {
@@ -51,7 +66,9 @@ public class BonfireState : MonoBehaviour
         if (turnOff) { extinguish(); turnOff = false; }
 
     }
+    #endregion
 
+    #region bonfireTick_IEnumerator
     //Called each tick to update campfire
     private IEnumerator bonfireTick() {
         while (true)
@@ -69,10 +86,14 @@ public class BonfireState : MonoBehaviour
         }
 
     }
+    #endregion
 
+    #region methods
     //lits up the bonfire
     public void lit() { hp = maxHp; this.transform.GetComponentInChildren<ParticleSystem>().Play(); state = BonfireState.states.encendida; }
+    public void lit(int modifier) { hp = maxHp + modifier; this.transform.GetComponentInChildren<ParticleSystem>().Play(); state = BonfireState.states.encendida; }
 
     //extinguish the bonfire
     private void extinguish() { hp = 0; this.transform.GetComponentInChildren<ParticleSystem>().Stop(); state = BonfireState.states.apagada; }
+    #endregion
 }
