0efc63591c0f03ecccb24614955f8e861274e4c7
paraxodon <pauperezroy@gmail.com>
2022-10-21 17:36:54 +0200

    New Camera System (automatic) new "getActiveCamera" string method
    estoy tope orgulloso, si quereis leer el codigo flipais uwu

--- Assets/Scripts/Managers/CameraMangement.cs
+++ Assets/Scripts/Managers/CameraMangement.cs
@@ -5,16 +5,43 @@ using Cinemachine;
 
 public class CameraMangement : MonoBehaviour
 {
-    [SerializeField] private CinemachineVirtualCamera[] vcams = new CinemachineVirtualCamera[3];
+    #region Singleton
+    public static CameraMangement instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    private CinemachineVirtualCamera[] vcams;
+    private CinemachineBlenderSettings blender;
+    [SerializeField] private float cameraChangeTime = 2;
     bool canChange = true;
 
+    private CinemachineVirtualCamera activeCamera;
 
     private void Start()
     {
+        vcams = GetComponentsInChildren<CinemachineVirtualCamera>();
+        GetComponentInChildren<CinemachineBrain>().m_CustomBlends =  ScriptableObject.CreateInstance<CinemachineBlenderSettings>();
+        blender = GetComponentInChildren<CinemachineBrain>().m_CustomBlends;
+        blender.m_CustomBlends = new CinemachineBlenderSettings.CustomBlend[vcams.Length * 2];
+
+        activeCamera = vcams[vcams.Length - 1];
+
+        int k = 0;
         for (int i = 0; i < vcams.Length; i++)
         {
             vcams[i].m_Priority = i;
-
+            for (int j = 1; j < 3; j++)
+            {
+                int num = i + j - 1 + k; //Este codigo es demasiado complejo como para explicarlo, en su momento lo supe (hace 20 segundos)
+                blender.m_CustomBlends[num].m_From = vcams[i].name;
+                blender.m_CustomBlends[num].m_To = vcams[i + j > vcams.Length - 1 ? i + j - vcams.Length : i + j].name;
+                blender.m_CustomBlends[num].m_Blend.m_Style = CinemachineBlendDefinition.Style.EaseInOut;
+                blender.m_CustomBlends[num].m_Blend.m_Time = cameraChangeTime;
+            }
+            k++;
         }
     }
 
@@ -33,7 +60,6 @@ public class CameraMangement : MonoBehaviour
     }
     public void SwitchCamera(int dir)
     {
-        //NOTE TO SELF: Make that camera changes don't stack
         int[] newPriority = new int[vcams.Length];
         bool isDirRight = dir > 0 ? true : false;
         int changeLength = isDirRight ? vcams.Length : 0;
@@ -51,15 +77,24 @@ public class CameraMangement : MonoBehaviour
         }
         for (int i = 0; i < vcams.Length; i++)
         {
+            if(newPriority[i] == vcams.Length - 1)
+            {
+                activeCamera = vcams[i];
+            }
             vcams[i].m_Priority = newPriority[i];
         }
         canChange = false;
-        StartCoroutine(waitForEndOfAnimation(1.5f));
+        StartCoroutine(waitForEndOfAnimation(0f));
     }
 
     IEnumerator waitForEndOfAnimation(float seconds)
     {
         yield return new WaitForSeconds(seconds);
         canChange = true;
+    }
+
+    public static string getActiveCamera()
+    {
+        return instance.activeCamera.name;
     }
 }
