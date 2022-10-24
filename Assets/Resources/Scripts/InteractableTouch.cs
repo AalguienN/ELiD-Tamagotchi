f3495efba09237bc96117ef83975beaeb8e1f857
otapogit <carlosotapo@gmail.com>
2022-10-24 12:39:41 +0200

    cambiados controles de mouse a touch
    eso

--- Assets/Resources/Scripts/InteractableTouch.cs
+++ Assets/Resources/Scripts/InteractableTouch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableResource : MonoBehaviour
+public class InteractableTouch : MonoBehaviour
 {
     #region Variables
     public Camera gameCamera;
@@ -26,7 +26,7 @@ public class InteractableResource : MonoBehaviour
     public bool tried = false;
     public bool visible = true;
 
-
+    Touch fingertouch;
     #endregion
 
     #region Start Set-up
@@ -39,7 +39,7 @@ public class InteractableResource : MonoBehaviour
         pixelH = gameCamera.pixelHeight;
 
         //default stick position
-        pointer = new Vector3((float)pixelW*0.5f,(float)pixelH*0.15f,3f);
+        pointer = new Vector3((float)pixelW * 0.5f, (float)pixelH * 0.15f, 3f);
         pixelPointer = new Vector3((float)pixelW * 0.5f, (float)pixelH * 0.15f, 0);
         sticky.transform.position = gameCamera.ScreenToWorldPoint(pointer);
 
@@ -50,11 +50,11 @@ public class InteractableResource : MonoBehaviour
         Input.multiTouchEnabled = false;
 
         //Defining campfire rectangle hitbox
-        rectangle = new Vector2(pixelW*.2f,pixelH*.3f);
-        rectangleCenter = new Vector3(pixelW* .5f, pixelH * .5f);
+        rectangle = new Vector2(pixelW * .2f, pixelH * .3f);
+        rectangleCenter = new Vector3(pixelW * .5f, pixelH * .5f);
         adjustZ = new Vector3(0, 0, 3f);
 
-        
+
         stickNum = SaveManager.getStickNum();
         stickNum = 2;
     }
@@ -67,15 +67,17 @@ public class InteractableResource : MonoBehaviour
     #region Actual Code
     void Update()
     {
-        if (busy|| !visible) { return; }
+        if (busy || !visible) { return; }
 
-        if (Input.GetMouseButtonUp(0))
+        fingertouch = Input.GetTouch(0);
+
+        if (fingertouch.phase == TouchPhase.Ended)
         {
             if (!hold) { return; }
             hold = false;
-            
+
             //check if rectangle
-            mousePos = Input.mousePosition;
+            mousePos = fingertouch.position;
             if (mousePos.x > (rectangleCenter.x - rectangle.x) && mousePos.x < (rectangleCenter.x + rectangle.x))
             {
                 if (mousePos.y > (rectangleCenter.y - rectangle.y) && mousePos.y < (rectangleCenter.y + rectangle.y))
@@ -109,14 +111,14 @@ public class InteractableResource : MonoBehaviour
 
         if (hold)
         {
-            mousePos = Input.mousePosition + adjustZ;
+            mousePos = new Vector3(fingertouch.position.x, fingertouch.position.y, adjustZ.z);
             sticky.transform.position = gameCamera.ScreenToWorldPoint(mousePos);
             return;
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (fingertouch.phase == TouchPhase.Began)
         {
-            mousePos = Input.mousePosition;
+            mousePos = fingertouch.position;
             if (mousePos.x > (pixelPointer.x - grabR) && mousePos.x < (pixelPointer.x + grabR))
             {
                 if (mousePos.y > (pixelPointer.y - grabR) && mousePos.y < (pixelPointer.y + grabR))
@@ -127,15 +129,17 @@ public class InteractableResource : MonoBehaviour
             }
         }
     }
-    
-    public IEnumerator ReturnStick() {
+
+    public IEnumerator ReturnStick()
+    {
 
         Vector3 currPos, moveDis;
         Vector3 pointerWorld = gameCamera.ScreenToWorldPoint(pointer);
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 40; i++)
+        {
             currPos = sticky.transform.position;
-            moveDis = pointerWorld-currPos;
-            sticky.transform.Translate(moveDis* .12f);
+            moveDis = pointerWorld - currPos;
+            sticky.transform.Translate(moveDis * .12f);
             yield return new WaitForEndOfFrame();
         }
         busy = false;
@@ -145,9 +149,9 @@ public class InteractableResource : MonoBehaviour
     {
         sticky.transform.position = gameCamera.ScreenToWorldPoint(pointer);
         int x = 60;
-        Vector3 scaleChange = new Vector3(0.1f/((float)x), .25f/((float)x), 0.1f/((float)x));
+        Vector3 scaleChange = new Vector3(0.1f / ((float)x), .25f / ((float)x), 0.1f / ((float)x));
         sticky.transform.localScale = new Vector3(0, 0, 0);
-        for(int i = 0; i<x; i++)
+        for (int i = 0; i < x; i++)
         {
             yield return new WaitForEndOfFrame();
             sticky.transform.localScale += scaleChange;
@@ -156,9 +160,13 @@ public class InteractableResource : MonoBehaviour
     }
     public void Consume()
     {
-       SaveManager.setStickNum(--stickNum);
+        SaveManager.setStickNum(--stickNum);
+        Debug.Log(stickNum + " palos restantes");
+    }
+    public void ConsumeBlue()
+    {
+        SaveManager.setStickNum(--stickNum);
         Debug.Log(stickNum + " palos restantes");
     }
     #endregion
-
 }
