65dd3607e69b3e4ff79a29ba75409372cbfe06f4
ByteDevES <byteplaysyt@gmail.com>
2022-11-13 00:30:05 +0100

    UI Fix

--- Assets/Resources/Scripts/Managers/UINumPalos.cs
+++ Assets/Resources/Scripts/Managers/UINumPalos.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UINumPalos : MonoBehaviour
@@ -8,6 +9,8 @@ public class UINumPalos : MonoBehaviour
     int savedSticks = -1;
 
     public TMP_Text stickText;
+    public GameObject child;
+    public float t = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,19 @@ public class UINumPalos : MonoBehaviour
     void Update()
     {
         int s = (SaveManager.getStickNum() >= 999) ? SaveManager.getStickNum() - 998 : SaveManager.getStickNum();
+        
+        if(s == 0)
+        {
+            t = Mathf.Lerp(t, 1, Time.deltaTime);
+        }   
+        else
+        {
+            t = Mathf.Lerp(t, 0, Time.deltaTime);
+        }
+
+        child.GetComponent<Image>().color = new Color(1,1,1,t);
+        child.GetComponentInChildren<TMP_Text>().color = new Color(1,1,1,t);
+
         if(savedSticks != s) {
             stickText.text = s.ToString();
         }
