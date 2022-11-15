117849a0a030e1dd6f950616642d66fbf5c50f77
ByteDevEs <byteplaysyt@gmail.com>
2022-11-15 08:48:48 +0100

    Intro loading script done

--- Assets/Resources/Scripts/Intro/IntroController.cs
+++ Assets/Resources/Scripts/Intro/IntroController.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class IntroController : MonoBehaviour
 {
-    public string sceneToLoad;
-    public float introDuration;
+    float introDuration;
+
+    public VideoClip introClip;
 
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadSceneAsync(sceneToLoad);
+        introDuration = (float)introClip.length + 0.5f;
+        print("Video duration: " + introDuration);
+        GameObject camera = Camera.main.gameObject;
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.targetCameraAlpha = 0.5F;
+        videoPlayer.clip = introClip;
+        videoPlayer.Play();
+        StartCoroutine(UnloadScene(introDuration));
+    }
+
+    IEnumerator UnloadScene(float introD)
+    {
+        yield return new WaitForSeconds(introD);
+        SceneManager.LoadSceneAsync(1);
+        SceneManager.UnloadSceneAsync(0);
     }
 
     // Update is called once per frame
